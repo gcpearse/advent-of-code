7fa06fdec44731e5c8df81523a24134f59ce6f56
gcpearse <glen.c.pearse@gmail.com>
2023-11-08 18:07:32 +0000

    reformatting

--- advent-2022/01-calorie-counting.js
+++ advent-2022/01-calorie-counting.js
@@ -1,37 +1,37 @@
 const fs = require('fs/promises');
 
 function countCalories() {
-    return fs.readFile(`${__dirname}/data/elves.txt`)
-        .then((data) => {
-            return data.toString().split('\n');
-        })
-        .then((entries) => {
-            let arrayOfArrays = [];
-            let tempArray = [];
-            for (let entry of entries) {
-                if (entry !== '') {
-                    tempArray.push(+entry);
-                } else {
-                    arrayOfArrays.push(tempArray);
-                    tempArray = [];
-                }
-            }
-            return arrayOfArrays;
-        })
-        .then((arrayOfArrays) => {
-            return arrayOfArrays.map(array => {
-                return array.reduce((a, b) => a + b);
-            });
-        })
-        .then((arrayOfTotals) => {
-            return Math.max(...arrayOfTotals);
-        });
+  return fs.readFile(`${__dirname}/data/elves.txt`)
+    .then((data) => {
+      return data.toString().split('\n');
+    })
+    .then((entries) => {
+      let arrayOfArrays = [];
+      let tempArray = [];
+      for (let entry of entries) {
+        if (entry !== '') {
+          tempArray.push(+entry);
+        } else {
+          arrayOfArrays.push(tempArray);
+          tempArray = [];
+        }
+      }
+      return arrayOfArrays;
+    })
+    .then((arrayOfArrays) => {
+      return arrayOfArrays.map(array => {
+        return array.reduce((a, b) => a + b);
+      });
+    })
+    .then((arrayOfTotals) => {
+      return Math.max(...arrayOfTotals);
+    });
 }
 
 countCalories()
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
