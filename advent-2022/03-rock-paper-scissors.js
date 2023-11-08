5f2de81fc6ad5c6122e1701d070e4a5dd7905331
gcpearse <glen.c.pearse@gmail.com>
2023-11-08 18:35:11 +0000

    writes function to calculate the total score after 2500 games of rock, paper, scissors according to the challenge rules

---
+++ advent-2022/03-rock-paper-scissors.js
@@ -0,0 +1,42 @@
+const fs = require('fs/promises');
+
+function calculateScore() {
+  return fs.readFile(`${__dirname}/data/strategy-guide.txt`)
+    .then((data) => {
+      return data.toString().split('\n');
+    })
+    .then((rounds) => {
+      const tally = {};
+      for (let round of rounds) {
+        if (!tally[round]) {
+          tally[round] = 1;
+        } else {
+          tally[round]++;
+        }
+      }
+      return tally;
+    })
+    .then((tally) => {
+      let score = 0;
+      for (let key in tally) {
+        if (key === 'A X') score += 4 * tally[key];
+        if (key === 'A Y') score += 8 * tally[key];
+        if (key === 'A Z') score += 3 * tally[key];
+        if (key === 'B X') score += 1 * tally[key];
+        if (key === 'B Y') score += 5 * tally[key];
+        if (key === 'B Z') score += 9 * tally[key];
+        if (key === 'C X') score += 7 * tally[key];
+        if (key === 'C Y') score += 2 * tally[key];
+        if (key === 'C Z') score += 6 * tally[key];
+      }
+      return score;
+    });
+}
+
+calculateScore()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
