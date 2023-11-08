d4b259a3500a6f2d558465aaa97e0e9e075c26b7
gcpearse <glen.c.pearse@gmail.com>
2023-11-08 18:08:14 +0000

    expands on function in challenge 01 to return the total of the top three summed totals

---
+++ advent-2022/02-top-three.js
@@ -0,0 +1,39 @@
+const fs = require('fs/promises');
+
+function countCalories() {
+  return fs.readFile(`${__dirname}/data/elves.txt`)
+    .then((data) => {
+      return data.toString().split('\n');
+    })
+    .then((entries) => {
+      let arrayOfArrays = [];
+      let tempArray = [];
+      for (let entry of entries) {
+        if (entry !== '') {
+          tempArray.push(+entry);
+        } else {
+          arrayOfArrays.push(tempArray);
+          tempArray = [];
+        }
+      }
+      return arrayOfArrays;
+    })
+    .then((arrayOfArrays) => {
+      return arrayOfArrays.map(array => {
+        return array.reduce((a, b) => a + b);
+      });
+    })
+    .then((arrayOfTotals) => {
+      const sortedTotals = arrayOfTotals.sort((a, b) => b - a);
+      const topThree = sortedTotals.splice(0, 3);
+      return topThree.reduce((a, b) => a + b);
+    });
+}
+
+countCalories()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
