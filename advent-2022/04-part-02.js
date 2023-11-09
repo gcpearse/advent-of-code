f409dbd84ced8cacf9a4bde3f679ec4751d1ad01
gcpearse <glen.c.pearse@gmail.com>
2023-11-09 00:14:38 +0000

    modifies function from part 1 to find the sum total of any overlapping ranges

---
+++ advent-2022/04-part-02.js
@@ -0,0 +1,38 @@
+const fs = require('fs/promises');
+
+function countOverlappingRanges() {
+  return fs.readFile(`${__dirname}/data/section-assignments.txt`)
+    .then((data) => {
+      return data.toString().split('\n');
+    })
+    .then((ranges) => {
+      return ranges.map(range => range.split(','));
+    })
+    .then((pairs) => {
+      return pairs.map(pair => {
+        return [
+          pair[0].split('-'),
+          pair[1].split('-')
+        ];
+      });
+    })
+    .then((nestedPairs) => {
+      let sum = 0;
+      nestedPairs.forEach(nestedPair => {
+        if (+nestedPair[0][1] < +nestedPair[1][0] || +nestedPair[0][0] > +nestedPair[1][1]) {
+          sum += 0;
+        } else {
+          sum++;
+        }
+      });
+      return sum;
+    });
+}
+
+countOverlappingRanges()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
