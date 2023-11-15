115477ea05171d9c7145cff006d52638d3fcca08
gcpearse <glen.c.pearse@gmail.com>
2023-11-15 19:16:28 +0000

    moves shared functionality to utils file

--- advent-2022/05-part-01.js
+++ advent-2022/05-part-01.js
@@ -1,46 +1,7 @@
+const { extractData, createStacks, createSteps, findTopCrates } = require("./utils/05-utils");
 const { getData } = require("./utils/general-utils");
 
 const path = `${__dirname}/data/rearrangement-procedure.txt`;
-
-function extractData(data) {
-  const crateRows = [];
-  const moves = [];
-  data.forEach(row => {
-    if (row.match(/[A-Z]/)) {
-      return crateRows.push(row);
-    }
-    if (row.match(/move \d+ from \d+ to \d+/)) {
-      return moves.push(row);
-    }
-  });
-  return Promise.all([crateRows, moves]);
-}
-
-function createstacks(crateRows, moves) {
-  const stacks = [];
-  for (let i = 1; i < crateRows[0].length; i += 4) {
-    const stack = [];
-    for (let row of crateRows) {
-      if (row[i] !== ' ') {
-        stack.push(row[i]);
-      }
-    }
-    stacks.push(stack);
-  }
-  return Promise.all([stacks, moves]);
-}
-
-function createSteps(stacks, moves) {
-  const split = moves.map((move) => {
-    return move.split(' ');
-  });
-  const steps = split.map((line) => {
-    return line.filter((item) => {
-      return item.match(/\d/);
-    });
-  });
-  return Promise.all([stacks, steps]);
-}
 
 function rearrangeCrates(stacks, steps) {
   for (let step of steps) {
@@ -54,18 +15,12 @@ function rearrangeCrates(stacks, steps) {
   return stacks;
 }
 
-function findTopCrates(stacks) {
-  let code = '';
-  stacks.forEach((stack) => code += stack[0]);
-  return code;
-}
-
 getData(path)
   .then((data) => {
     return extractData(data);
   })
   .then(([crateRows, moves]) => {
-    return createstacks(crateRows, moves);
+    return createStacks(crateRows, moves);
   })
   .then(([stacks, moves]) => {
     return createSteps(stacks, moves);
