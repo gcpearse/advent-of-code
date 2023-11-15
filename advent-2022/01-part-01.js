008112863f7bdc6944271de74d7a695f2f2baae7
gcpearse <glen.c.pearse@gmail.com>
2023-11-15 15:32:34 +0000

    refactoring to separate concerns, renames functions and variables

--- advent-2022/01-part-01.js
+++ advent-2022/01-part-01.js
@@ -1,34 +1,46 @@
 const fs = require('fs/promises');
 
-function countCalories() {
+function getCalorieData() {
   return fs.readFile(`${__dirname}/data/elf-calories.txt`)
     .then((data) => {
       return data.toString().split('\n');
-    })
-    .then((entries) => {
-      const arrayOfArrays = [];
-      let tempArray = [];
-      for (let entry of entries) {
-        if (entry !== '') {
-          tempArray.push(+entry);
-        } else {
-          arrayOfArrays.push(tempArray);
-          tempArray = [];
-        }
-      }
-      return arrayOfArrays;
-    })
-    .then((arrayOfArrays) => {
-      return arrayOfArrays.map(array => {
-        return array.reduce((a, b) => a + b);
-      });
-    })
-    .then((arrayOfTotals) => {
-      return Math.max(...arrayOfTotals);
     });
 }
 
-countCalories()
+function createArrayOfArrays(items) {
+  const arrayOfArrays = [];
+  let tempArray = [];
+  for (let item of items) {
+    if (item !== '') {
+      tempArray.push(+item);
+    } else {
+      arrayOfArrays.push(tempArray);
+      tempArray = [];
+    }
+  }
+  return arrayOfArrays;
+}
+
+function findTotals(arrayOfArrays) {
+  return arrayOfArrays.map(array => {
+    return array.reduce((a, b) => a + b);
+  });
+}
+
+function findHighestTotal(arrayOfTotals) {
+  return Math.max(...arrayOfTotals);
+}
+
+getCalorieData()
+  .then((data) => {
+    return createArrayOfArrays(data);
+  })
+  .then((result) => {
+    return findTotals(result);
+  })
+  .then((result) => {
+    return findHighestTotal(result);
+  })
   .then((result) => {
     console.log(result);
   })
