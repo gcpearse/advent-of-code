11bcb9ec561c6b066615a0131de846cce2a517f5
gcpearse <glen.c.pearse@gmail.com>
2023-11-15 16:07:57 +0000

    refactoring to separate concerns and move shared functionality into utils

--- advent-2022/02-part-02.js
+++ advent-2022/02-part-02.js
@@ -1,39 +1,36 @@
 const fs = require('fs/promises');
+const { tallyRounds } = require('./utils/02-utils');
 
-function calculateScore() {
+function getStrategyData() {
   return fs.readFile(`${__dirname}/data/strategy-guide.txt`)
     .then((data) => {
       return data.toString().split('\n');
-    })
-    .then((rounds) => {
-      const tally = {};
-      for (let round of rounds) {
-        if (!tally[round]) {
-          tally[round] = 1;
-        } else {
-          tally[round]++;
-        }
-      }
-      return tally;
-    })
-    .then((tally) => {
-      let score = 0;
-      for (let key in tally) {
-        if (key === 'B X') score += 1 * tally[key]; // ok
-        if (key === 'C X') score += 2 * tally[key]; // ok
-        if (key === 'A X') score += 3 * tally[key]; // ok
-        if (key === 'A Y') score += 4 * tally[key]; // ok
-        if (key === 'B Y') score += 5 * tally[key]; // ok
-        if (key === 'C Y') score += 6 * tally[key]; // ok
-        if (key === 'C Z') score += 7 * tally[key]; // ok
-        if (key === 'A Z') score += 8 * tally[key]; // ok
-        if (key === 'B Z') score += 9 * tally[key]; // ok
-      }
-      return score;
     });
 }
 
-calculateScore()
+function getScore(tally) {
+  let score = 0;
+  for (let key in tally) {
+    if (key === 'B X') score += 1 * tally[key];
+    if (key === 'C X') score += 2 * tally[key];
+    if (key === 'A X') score += 3 * tally[key];
+    if (key === 'A Y') score += 4 * tally[key];
+    if (key === 'B Y') score += 5 * tally[key];
+    if (key === 'C Y') score += 6 * tally[key];
+    if (key === 'C Z') score += 7 * tally[key];
+    if (key === 'A Z') score += 8 * tally[key];
+    if (key === 'B Z') score += 9 * tally[key];
+  }
+  return score;
+}
+
+getStrategyData()
+  .then((data) => {
+    return tallyRounds(data);
+  })
+  .then((result) => {
+    return getScore(result);
+  })
   .then((result) => {
     console.log(result);
   })
