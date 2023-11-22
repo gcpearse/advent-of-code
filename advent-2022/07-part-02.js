2e4ddd479f4c9f1b718c9ae683f32e20bd2729ba
gcpearse <glen.c.pearse@gmail.com>
2023-11-22 23:05:34 +0000

    moves shared functionality to utils file

--- advent-2022/07-part-02.js
+++ advent-2022/07-part-02.js
@@ -1,42 +1,7 @@
+const { getDirectories, splitData } = require("./utils/07-utils");
 const { getData } = require("./utils/general-utils");
 
 const path = `${__dirname}/data/terminal-output.txt`;
-
-function splitData(data) {
-  return data.map((item) => item.split(' '));
-}
-
-function getDirectories(data) {
-  let path = "";
-  const directories = {
-    "/root": 0
-  };
-  for (let item of data) {
-    if (item[1] !== "ls" && item[0] !== "dir") {
-      if (item[0] === "$") {
-        if (item[1] === "cd") {
-          if (item[2] === "..") {
-            path = path.slice(0, path.lastIndexOf("/"));
-          } else if (item[2] === "/") {
-            path = "/root";
-          } else {
-            path = `${path}/${item[2]}`;
-            directories[path] = 0;
-          }
-        }
-      } else {
-        if (item[0] !== "dir") {
-          let temp = path;
-          while (temp !== "") {
-            directories[temp] += +item[0];
-            temp = temp.slice(0, temp.lastIndexOf("/"));
-          }
-        }
-      }
-    }
-  }
-  return directories;
-}
 
 function findDirectoryToDelete(directories) {
   const usedSpace = directories["/root"];
