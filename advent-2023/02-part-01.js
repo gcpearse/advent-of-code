6b03a786ec8e6af5adcdd4a4376148992064b1d1
gcpearse <glen.c.pearse@gmail.com>
2023-12-03 17:06:07 +0000

    moves shared functionality to utils

--- advent-2023/02-part-01.js
+++ advent-2023/02-part-01.js
@@ -1,45 +1,7 @@
+const { sliceData, parseGames, tallyCubes } = require("./utils/02-utils");
 const { getData } = require("./utils/general-utils");
 
 const path = `${__dirname}/data/game-record.txt`;
-
-function sliceData(data) {
-  return data.map((line) => {
-    return line.slice(line.indexOf(":") + 2);
-  });
-}
-
-function parseGames(data) {
-  const parsedData = data.map((line) => {
-    return line
-      .replaceAll(";", ",");
-  });
-  const games = parsedData.map((line) => {
-    return line.split(", ");
-  });
-  return games.map((game) => {
-    return game.map((move) => {
-      return move.split(" ");
-    });
-  });
-}
-
-function tallyCubes(games) {
-  const result = [];
-  for (let game of games) {
-    const tally = {
-      red: 0,
-      green: 0,
-      blue: 0
-    };
-    for (let i = 0; i < game.length; i++) {
-      if (+game[i][0] > tally[game[i][1]]) {
-        tally[game[i][1]] = +game[i][0];
-      }
-    }
-    result.push(tally);
-  }
-  return result;
-}
 
 function findPossibleGames(tallies) {
   let result = 0;
