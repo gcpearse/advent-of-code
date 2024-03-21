83d1a9d9605d8257e82ee6b677fc6753046e4d2e
gcpearse <glen.c.pearse@gmail.com>
2024-03-21 09:46:26 +0000

    converts file to TypeScript

---
+++ advent-2022/05-part-02.ts
@@ -0,0 +1,48 @@
+import { extractData, createStacks, createSteps, findTopCrates } from "./utils/05-utils"
+import { getData } from "./utils/general-utils"
+
+
+const path = `${__dirname}/../advent-2022/data/rearrangement-procedure.txt`
+
+
+function rearrangeCrates(stacks: string[][], steps: string[][]): string[][] {
+
+  for (const step of steps) {
+
+    const move = +step[0]
+
+    const from = +step[1] - 1
+
+    const to = +step[2] - 1
+
+    const crates = stacks[from].splice(0, move)
+
+    stacks[to].unshift(...crates)
+  }
+
+  return stacks
+}
+
+
+getData(path)
+  .then((data) => {
+    return extractData(data)
+  })
+  .then(([crateRows, moves]) => {
+    return createStacks(crateRows, moves)
+  })
+  .then(([stacks, moves]) => {
+    return createSteps(stacks, moves)
+  })
+  .then(([stacks, steps]) => {
+    return rearrangeCrates(stacks, steps)
+  })
+  .then((result) => {
+    return findTopCrates(result)
+  })
+  .then((result) => {
+    console.log(result)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
